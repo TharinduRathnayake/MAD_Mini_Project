409c600d0d49ca0db7d0405385da245eb5134e75
SUra1212 <89268636+SUra1212@users.noreply.github.com>
2021-09-26 01:02:35 +0530

    login setting

--- app/src/main/java/com/example/suwa_arana/MainActivity.java
+++ app/src/main/java/com/example/suwa_arana/MainActivity.java
@@ -2,13 +2,54 @@ package com.example.suwa_arana;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button drRegister;
+    private Button drChanneling;
+    private Button signOut;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drRegister = findViewById(R.id.dr_register);
+        drChanneling = findViewById(R.id.dr_channeling);
+        signOut = findViewById(R.id.btn_signOut);
+        mAuth = FirebaseAuth.getInstance();
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                startActivity(new Intent(MainActivity.this, SingIn.class));
+                finish();
+            }
+        });
+
+        drRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterForm.class));
+            }
+        });
+
+        drChanneling.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ChannelActivity.class));
+            }
+        });
+
+
+
     }
 }
